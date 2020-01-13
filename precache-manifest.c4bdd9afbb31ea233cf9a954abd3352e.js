892194bcbf3939c0748b4676a73f4320508e3c27
rishichawla <rishi.chawla14@gmail.com>
2020-01-14 01:46:29 +0530

    Updates

---
+++ precache-manifest.c4bdd9afbb31ea233cf9a954abd3352e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d132199ee7211a2b38c04968ef82b8a7",
+    "url": "/GetYourWeb/index.html"
+  },
+  {
+    "revision": "a2326d4bd13964248448",
+    "url": "/GetYourWeb/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5063529dc8d08aa49b19",
+    "url": "/GetYourWeb/static/js/2.0456cee9.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/GetYourWeb/static/js/2.0456cee9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2326d4bd13964248448",
+    "url": "/GetYourWeb/static/js/main.2b428cff.chunk.js"
+  },
+  {
+    "revision": "d4a87e7058810afb0b59",
+    "url": "/GetYourWeb/static/js/runtime-main.790c6bdc.js"
+  }
+]);
