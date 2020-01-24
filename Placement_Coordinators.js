f1ee9f90371feabf24176a212744fe6c70929244
Sakshi Maheshwari <sakshimaheshwari10@gmail.com>
2020-01-24 22:41:08 +0530

    contact_us updated

---
+++ Placement_Coordinators.js
@@ -0,0 +1,8 @@
+var placementCoordinators = ['{"Name":"Sakshi","Email_id":"cnksk","Branch":"Information Technology","Year":"3rd year"}',
+                            '{"Name":"Akash","Email_id":"bdasijx","Branch":"Information Technology","Year":"3rd year"}',
+                            '{"Name":"rishi ","Email_id":"csjc","Branch":"Information Technology","Year":"3rd year"}',
+                            '{"Name":"Arundhati","Email_id":"cds","Branch":"Information Technology","Year":"3rd year"}',
+                            '{"Name":"Vinayak","Email_id":"bdis","Branch":"Information Technology","Year":"3rd year"}',
+                            '{"Name":"Shruti","Email_id":"bxbbb","Branch":"Information Technology","Year":"3rd year"}',
+                            '{"Name":"Manas Jain","Email_id":"bhjjb","Branch":"Computer Science","Year":"4th year"}']
+   
